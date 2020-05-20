e29da019e8e113290cf96e65863fd2493516dae2
Luka <luka.pilic5@gmail.com>
2020-05-20 15:00:04 +0200

    init

---
+++ app/src/main/java/at/htlgkr/tournamaker/MainActivity.java
@@ -0,0 +1,26 @@
+package at.htlgkr.tournamaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "BRUH MOMENT", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
